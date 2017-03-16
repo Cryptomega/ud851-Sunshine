9fe26f9cd311d43803f689c41a730284fe3a246e
Philip Schexnayder <philipschexnayder@gmail.com>
2017-03-15 19:02:05 -0500

    exercises

---
+++ MyApplication/app/src/main/java/com/example/android/myapplication/main.java
@@ -0,0 +1,14 @@
+package com.example.android.myapplication;
+
+
+/**
+ * Created by Philip on 3/10/2017.
+ */
+
+public class main
+{
+    public static void main ()
+    {
+        System.out.print("Goodbye world!");
+    }
+}
--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,15 +17,21 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a field to store the weather display TextView
+    TextView textViewWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        textViewWeather = (TextView) findViewById(R.id.tv_weather_data);
+
+        String[] fakeWeatherData;
 
         // TODO (2) Use findViewById to get a reference to the weather display TextView
 
